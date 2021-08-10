ea3cee41db1c1749ebbb261097ebb183b8284cff
yli310 <77583949+yli310@users.noreply.github.com>
2021-08-09 22:50:54 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -60,11 +60,11 @@ def process_order(order):
 
     
     existing = g.session.query(Order).filter(Order.filled == None, Order.buy_currency == order.sell_currency,Order.sell_currency == order.buy_currency, Order.sell_amount/Order.buy_amount >= exchange_rate).first()
-
+    order.filled = datetime.now()
     #filled
     if existing!=None:
       existing.filled = datetime.now()
-      order.filled = datetime.now()
+      
       #counterparty id
       order.counterparty_id = existing.id
       existing.counterparty_id = order.id
